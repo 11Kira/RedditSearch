07379180a4de69f1b973431a67a12b9a70d114c9
julius.villagracia <kiravillagracia@yahoo.com>
2021-08-21 20:31:36 +0800

    #ui: added initial functions for recyclerview, dataBinding for main layout

--- app/src/main/java/com/test/redditsearch/MainActivity.kt
+++ app/src/main/java/com/test/redditsearch/MainActivity.kt
@@ -2,10 +2,30 @@ package com.test.redditsearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.test.redditsearch.databinding.ActivityMainBinding
 
+/**
+ * Main activity class
+ * @author Julius Villagracia
+ */
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    /**
+     * Initializes the recyclerView
+     */
+    private fun initRecyclerView() {
+        binding.repoList.apply {
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+        }
     }
 }
